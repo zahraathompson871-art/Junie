372d94217216f609cbae7cf5e362bc9865241258
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-18 15:44:33 +0200

    added create and delete user conrollers and bcrypt

--- backend/controllers/userCon.js
+++ backend/controllers/userCon.js
@@ -1,4 +1,5 @@
 import * as userModel from "../models/userModel.js";
+import bcrypt from "bcrypt";
 
 export const getUsers = async (req, res, next) => {
     try{
@@ -18,3 +19,42 @@ export const getUser = async (req, res, next) => {
         next(err);
     }
 };
+
+export const createUser = async (req, res, next) => {
+    try{
+        const {full_name, email, password} = req.body;
+
+
+        if (!full_name || !email || !password){
+            return res.status(400).json({error:"All fields are required"});
+        }
+
+        const password_hash= await bcrypt.hash(password,10);
+
+        const newUser = await userModel.createUser({full_name, email, password_hash});
+        
+        res.status(201).json(newUser);
+    }catch (err){
+        next(err);
+    }
+};
+
+export const deleteUserByEmail = async (req,res,next) => {
+    try {
+        const {email} = req.body;
+
+        if (!email){
+            return res.status(400).json({error: "Email is required"});
+        }
+
+        const deleted = await userModel.deleteUserByEmail(email);
+
+        if(!deleted){
+            return res.status(404).json({error:"User not found"});
+        }
+
+        res.status(200).json({message:"User deleted successfully"});
+    }catch(err){
+        next(err)
+    }
+}
