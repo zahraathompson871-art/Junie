f227ce3d0b58d3d3c39f13116a062e6f672d584f
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-18 10:26:43 +0200

    removed error handling and fixed imports

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,16 @@
-const mysql = require("mysql2");
-require("dotenv").config();
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-const connection = mysql.createConnection({
+dotenv.config();
+
+export const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
 });
 
-connection.connect((err)=>{
-    if(err){
-        console.error("Error connecting to DB:", err);
-        return;
-    }
-    console.log("Connected to MySql db");
-});
+console.log("MySQL pool created successfully");
 
-module.exports = connection
