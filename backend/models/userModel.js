ba4d04da86367e83e12754b18d114c013756bf18
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-18 15:43:31 +0200

    Added create and delete user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -9,4 +9,17 @@ export const getAllUsers = async () => {
 export const getUserById = async (id) => {
     const [rows] = await pool.query("SELECT * FROM users WHERE users_id = ?", [id]);
     return rows[0];
+};
+
+export const createUser = async (data) => {
+    const {full_name, email, password_hash} = data;
+    const [result] = await pool.query("INSERT INTO users (full_name, email, password_hash) VALUES (?,?,?)",[full_name, email,password_hash]);
+    return { id: result.insertId, full_name, email};
+};
+
+export const deleteUserByEmail = async (email) => {
+    const [result] = await pool.query(
+        "DELETE FROM users WHERE email = ?",[email]
+    );
+    return result.affectedRows;
 }
