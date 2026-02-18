013c2361ef965ca8374f72db40cead310bbc4161
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-18 15:45:23 +0200

    delete and post router

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import{getUsers, getUser} from "../controllers/userCon.js"
+import{getUsers, getUser, createUser, deleteUserByEmail} from "../controllers/userCon.js"
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.get("/:id",getUser);
+router.post("/",createUser);
+router.delete("/", deleteUserByEmail);
 
 export default router;
