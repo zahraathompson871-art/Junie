0aedb5906aa54ae47ffbe2751c30ea6a5eda7458
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-23 16:03:11 +0200

    integration

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import{getUsers, getUser, createUser, deleteUserByEmail, loginUser, getCurrentUser} from "../controllers/userCon.js"
+import{getUsers, getUser, createUser, deleteUserByEmail, loginUser, getCurrentUser, updateUser} from "../controllers/userCon.js"
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", protect, getUsers);
 router.get("/me", protect, getCurrentUser);
+router.put("/me", protect, updateUser)
 router.get("/:id",getUser);
 router.post("/",createUser);
 router.delete("/", deleteUserByEmail);
