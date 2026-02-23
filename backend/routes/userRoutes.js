9091ff03dbad5d1381640c660fe7587863b7dbb9
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-23 12:29:35 +0200

    added getCurrentUser

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
-import{getUsers, getUser, createUser, deleteUserByEmail, loginUser} from "../controllers/userCon.js"
+import{getUsers, getUser, createUser, deleteUserByEmail, loginUser, getCurrentUser} from "../controllers/userCon.js"
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", protect, getUsers);
+router.get("/me", protect, getCurrentUser);
 router.get("/:id",getUser);
 router.post("/",createUser);
 router.delete("/", deleteUserByEmail);
 router.post("/login", loginUser);
+
 
 export default router;
