330254a6222463c9e50115993e0f2ca2c253e7e4
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-23 06:21:20 +0200

    authentication

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+    try{
+        const authHeader = req.headers.authorization;
+
+        if(!authHeader || !authHeader.startWith("Bearer")){
+            return res.status(401).json({error:"Not authorized"});
+        }
+        const token = authHeader.spli("")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
+
+        next();
+    }catch(err){
+        res.status(401).json({error: "Invalid or expired token"});
+    }
+};
