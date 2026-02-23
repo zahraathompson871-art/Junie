2ff10f38eb9c6ed5703fbcf6d86cbe514498e8e5
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-23 16:02:35 +0200

    integration

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,9 +8,10 @@ export const protect = (req, res, next) => {
             return res.status(401).json({error:"Not authorized"});
         }
         const token = authHeader.spli("")[1];
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.user = decoded;
+        req.user = { id: decoded.id, email: decoded.email};
 
         next();
     }catch(err){
