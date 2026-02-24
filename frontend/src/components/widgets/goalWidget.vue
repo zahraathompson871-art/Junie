3568f6c44ac4566a6b5f3eca03e312f3be8f9b74
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-24 16:03:22 +0200

    initial setup

---
+++ frontend/src/components/widgets/goalWidget.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="widget-card">
+        <h5>Goals</h5>
+        <p>{{ data.target }}</p>
+        <div class="progress">
+            <div class="progress-bar"
+                 :style="{width: percentage + '%'}">
+                {{ data.current }}/{{ data.total }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['data'],
+    computed: {
+        percentage() {
+            if (this.data.total) return 0
+                return (this.data.current / this.data.total) * 100 
+        }
+    }
+}
+</script>
