d37f5e2722394f682086ca8bc59d7cbe909d17e2
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-24 16:04:19 +0200

    fixed errors

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,10 +4,10 @@ export const protect = (req, res, next) => {
     try{
         const authHeader = req.headers.authorization;
 
-        if(!authHeader || !authHeader.startWith("Bearer")){
+        if(!authHeader || !authHeader.startsWith("Bearer")){
             return res.status(401).json({error:"Not authorized"});
         }
-        const token = authHeader.spli("")[1];
+        const token = authHeader.split(" ")[1];
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
