6c6790e94942bc6e5f9c0c673d43d96b4ff5e914
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-25 16:06:39 +0200

    added templates routes

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,22 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-dotenv.config();
-import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import templateRoutes from "./routes/tempRoutes.js";
+import checkoutRoutes from "./routes/checkout.js";
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
 
-
+dotenv.config();
 
 const app = express();
 
-app.use(cors({origin: "http://localhost:5173"}));
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use("/api/templates", templateRoutes);
+app.use("/api/paypal", checkoutRoutes);
+app.use("/api/checkout", checkoutRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
@@ -20,5 +24,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running in port ${PORT}`);
-})
+  console.log(`Server running in port ${PORT}`);
+});
