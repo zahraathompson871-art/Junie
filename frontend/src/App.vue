523cf23659cfcb780979e3af7e2acee41ef9a006
Zahraa Thompson <zahraathompson871@gmail.com>
2026-02-26 07:35:37 +0200

    footer added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="light-theme">
+  <div class="light-theme app-shell">
     <TopBar />
-    <router-view />
+    <main class="app-content">
+      <router-view />
+    </main>
+    <SiteFooter />
   </div>
 </template>
 
 <script>
 import TopBar from './components/TopBar.vue'
+import SiteFooter from './components/SiteFooter.vue'
 
 export default {
-  components: { TopBar }
+  components: { TopBar, SiteFooter }
 }
 </script>
+
+<style scoped>
+.app-shell {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.app-content {
+  flex: 1;
+}
+</style>
 
